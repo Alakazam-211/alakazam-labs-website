7727be86e1db464c62acf285cdb9b2a63cb9e829
z3thon <rosson.long@lztek.io>
2025-12-20 18:22:17 -0700

    Increase MagicRibbon max-width to prevent glow cutoff
    - Changed max-w-4xl to max-w-6xl to allow more room for glow effect
    - Prevents ribbon glow from being cut off when weaving to edges

--- components/MagicRibbon.tsx
+++ components/MagicRibbon.tsx
@@ -20,7 +20,7 @@ export default function MagicRibbon() {
   }, []);
 
   return (
-    <div className="absolute left-1/2 top-0 pointer-events-none z-0 -translate-x-1/2 w-full max-w-4xl" style={{ height: '100%', minHeight: '100vh' }}>
+    <div className="absolute left-1/2 top-0 pointer-events-none z-0 -translate-x-1/2 w-full max-w-6xl" style={{ height: '100%', minHeight: '100vh' }}>
       <svg
         className="w-full h-full"
         viewBox="0 0 400 2000"
